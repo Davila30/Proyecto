2ff659e45d19d2a734f1a71da0c3f865c3df614e
Davila30 <avilamks12@gmail.com>
2023-12-09 15:11:19 +0100

    jsmore

--- proyecto/scripts/script.js
+++ proyecto/scripts/script.js
@@ -5,14 +5,13 @@ function myFunction() {
     } else {
         x.className = "topnav";
     }
-}
-
-var links = document.querySelectorAll('.topnav a[href^="#"]');
-links.forEach(function(link) {
-    link.addEventListener('click', function() {
-        var x = document.getElementById("myTopnav");
-        if (x.className.includes("responsive")) {
-            x.className = "topnav";
-        }
+    
+    // Cierra la barra de navegación cuando se hace clic en un enlace interno
+    var links = document.querySelectorAll('.topnav a[href^="#"]');
+    links.forEach(function(link) {
+        link.addEventListener('click', function() {
+            var x = document.getElementById("myTopnav");
+            x.className = "topnav"; // Cambia la clase para cerrar el menú
+        });
     });
-});
+}
